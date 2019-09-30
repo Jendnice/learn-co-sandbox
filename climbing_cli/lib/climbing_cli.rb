701ade9ef3fafa49d6bbe91ebe56ebab9863e607
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-30 20:26:13 +0000

    first commit

---
+++ climbing_cli/lib/climbing_cli.rb
@@ -0,0 +1,6 @@
+require "climbing_cli/version"
+
+module ClimbingCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ climbing_cli/lib/climbing_cli/version.rb
@@ -0,0 +1,3 @@
+module ClimbingCli
+  VERSION = "0.1.0"
+end
