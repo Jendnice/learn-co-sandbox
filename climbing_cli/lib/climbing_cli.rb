ed4aa11042125230494400a317c83e005663ca44
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-01 02:26:17 +0000

    setting up the framework

--- climbing_cli/lib/climbing_cli.rb
+++ climbing_cli/lib/climbing_cli.rb
@@ -1,6 +1,7 @@
-require "climbing_cli/version"
+require_relative "climbing_cli/version"
+require_relative "climbing_cli/cli"
 
-module ClimbingCli
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module ClimbingCli
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
---
+++ climbing_cli/lib/climbing_cli/cli.rb
@@ -0,0 +1,7 @@
+class ClimbingCli::CLI
+  
+  def call 
+    puts "Today's climbs!"
+  end
+  
+end 
