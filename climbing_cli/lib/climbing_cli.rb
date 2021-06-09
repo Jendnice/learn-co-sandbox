0ea1b6f03263e1daaf4b257fa7bdd631baf6400a
Jennifer Nicely <pipeline38@hotmail.com>
2021-06-09 19:15:03 +0000

    Automatically backed up by Learn

--- climbing_cli/lib/climbing_cli.rb
+++
@@ -1,7 +0,0 @@
-require_relative "climbing_cli/version"
-require_relative "climbing_cli/cli"
-
-# module ClimbingCli
-#   class Error < StandardError; end
-#   # Your code goes here...
-# end
--- climbing_cli/lib/climbing_cli/version.rb
+++
@@ -1,3 +0,0 @@
-module ClimbingCli
-  VERSION = "0.1.0"
-end
